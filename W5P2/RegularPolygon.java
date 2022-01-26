cea63cd3916cc0df4afdf80a132b6092ac7e7613
Tee <82800595+Teerawat36167@users.noreply.github.com>
2022-01-26 15:57:35 +0700

    Week 5 Program 2

--- W5P2/RegularPolygon.java
+++ W5P2/RegularPolygon.java
@@ -1,3 +1,31 @@
 public class RegularPolygon {
-    
+    private int n = 3;
+    private double side = 1;
+    private double x = 0;
+    private double y = 0;
+
+    public RegularPolygon() {
+
+    }
+
+    public RegularPolygon(int n,double side) {
+        this.n = n;
+        this.side = side;
+    }
+
+    public int accessorN() {
+        return this.n;
+    }
+
+    public double accessorSide() {
+        return this.side;
+    }
+
+    public double accessorX() {
+        return x;
+    }
+
+    public double accessorY() {
+        return y;
+    }
 }
--- W5P2/W5P2.java
+++ W5P2/W5P2.java
@@ -1,5 +1,5 @@
 public class W5P2 {
     public static void main(String[] args) {
-        
+        RegularPolygon test = new RegularPolygon(3,4);
     }
 }
