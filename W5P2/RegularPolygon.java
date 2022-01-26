f37638c294d57421cec83f738556edf1935bb163
Tee <82800595+Teerawat36167@users.noreply.github.com>
2022-01-26 19:41:10 +0700

    Week 5 Program 2

--- W5P2/RegularPolygon.java
+++ W5P2/RegularPolygon.java
@@ -1,8 +1,12 @@
+import java.lang.Math;
+
 public class RegularPolygon {
     private int n = 3;
     private double side = 1;
     private double x = 0;
     private double y = 0;
+
+    // Constructor
 
     public RegularPolygon() {
 
@@ -12,6 +16,15 @@ public class RegularPolygon {
         this.n = n;
         this.side = side;
     }
+
+    public RegularPolygon(int n,double side,double x,double y) {
+        this.n = n;
+        this.side = side;
+        this.x = x;
+        this.y = y;
+    }
+
+    // Method
 
     public int accessorN() {
         return this.n;
@@ -27,5 +40,29 @@ public class RegularPolygon {
 
     public double accessorY() {
         return y;
+    }
+
+    public void mutatorN(int n) {
+        this.n = n;
+    }
+
+    public void mutatorSide(double side) {
+        this.side = side;
+    }
+
+    public void mutatorX(double x) {
+        this.x = x;
+    }
+
+    public void mutatorY(double y) {
+        this.y = y;
+    }
+
+    public double getPerimeter() {
+        return n*side;
+    }
+
+    public double getArea() {
+        return (n*(side*side))/(4*Math.tan(Math.PI/n));
     }
 }
--- W5P2/W5P2.java
+++ W5P2/W5P2.java
@@ -1,5 +1,13 @@
 public class W5P2 {
     public static void main(String[] args) {
-        RegularPolygon test = new RegularPolygon(3,4);
+        RegularPolygon P1 = new RegularPolygon();
+        RegularPolygon P2 = new RegularPolygon(6,4);
+        RegularPolygon P3 = new RegularPolygon(10,4,5.6,7.8);
+        System.out.println("Perimeter P1 = "+P1.getPerimeter());
+        System.out.println("Area P1 = "+P1.getArea());
+        System.out.println("Perimeter P2 = "+P2.getPerimeter());
+        System.out.println("Area P2 = "+P2.getArea());
+        System.out.println("Perimeter P3 = "+P3.getPerimeter());
+        System.out.println("Area P3 = "+P3.getArea());
     }
 }
