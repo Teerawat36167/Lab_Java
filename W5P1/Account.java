8cc7c06196493439f90b6b7e882cf5be477546c3
Tee <82800595+Teerawat36167@users.noreply.github.com>
2022-01-26 15:39:47 +0700

    Week5 Program1

--- W5P1/Account.java
+++ W5P1/Account.java
@@ -1,4 +1,4 @@
-import java.sql.Date;
+import java.util.*;
 
 public class Account {
     private int id = 0;
@@ -7,7 +7,13 @@ public class Account {
     private Date dateCreated;
 
     public Account() {
-        System.out.println("Create account complete");
+        dateCreated = new Date();
+    }
+
+    public Account(int id,double balance) {
+        this.id = id;
+        this.balance = balance;
+        dateCreated = new Date();
     }
 
     public int accessorId() {
@@ -22,6 +28,10 @@ public class Account {
         return this.annualInterestRate;
     }
 
+    public Date accessorDateCreated() {
+        return this.dateCreated;
+    }
+
     public void mutatorId (int newId) {
         this.id = newId;
     }
@@ -32,5 +42,21 @@ public class Account {
 
     public void mutatorAnnualInterestRate (double newAnnualInterestRate) {
         this.annualInterestRate = newAnnualInterestRate;
+    }
+
+    public double getMonthlyInterestRate() {
+        return (this.annualInterestRate/12)/100;
+    }
+
+    public double getMonthlyInterest() {
+        return this.balance*getMonthlyInterestRate();
+    }
+
+    public void withdraw(double amount) {
+        this.balance -= amount;
+    }
+
+    public void deposit(double amount) {
+        this.balance += amount;
     }
 }
--- W5P1/W5P1.java
+++ W5P1/W5P1.java
@@ -1,5 +1,11 @@
 public class W5P1 {
     public static void main(String[] args) {
-        Account tee = new Account();
+        Account ac1 = new Account(1122,20000);
+        ac1.mutatorAnnualInterestRate(4.5);
+        ac1.withdraw(2500);
+        ac1.deposit(3000);
+        System.out.println("Balance = "+ac1.accessorBalance()+"$");
+        System.out.println("Monthly interest = "+ac1.getMonthlyInterest());
+        System.out.println("Date = "+ac1.accessorDateCreated());
     }
 }
