076088d7c8761581794dda2a20c1841d478fa580
Tee <82800595+Teerawat36167@users.noreply.github.com>
2022-02-03 22:35:03 +0700

    Week6 Program4

--- W6P4/ComputeTax.java
+++ W6P4/ComputeTax.java
@@ -17,12 +17,40 @@ public class ComputeTax {
     }
 
     public double tax(int i, int income) {
-        double tax = brackets[i][0] * rates[0] +
-                    (brackets[i][1] - brackets[i][0]) * rates[1] +
-                    (brackets[i][2] - brackets[i][1]) * rates[2] +
-                    (brackets[i][3] - brackets[i][2]) * rates[3] +
-                    (brackets[i][4] - brackets[i][3]) * rates[4] +
-                    (income - brackets[i][4]) * rates[5];
+        double tax = 0;
+        if (income <= brackets[i][0]) {
+            tax = income * rates[0];
+        }
+        else if (income <= brackets[i][1]) {
+            tax = brackets[i][0] * rates[0] +
+                (income - brackets[i][0]) * rates[1];
+        }
+        else if (income <= brackets[i][2]) {
+            tax = brackets[i][0] * rates[0] +
+                (brackets[i][1] - brackets[i][0]) * rates[1] +
+                (income - brackets[i][1]) * rates[2];
+        }
+        else if (income <= brackets[i][3]) {
+            tax = brackets[i][0] * rates[0] +
+                (brackets[i][1] - brackets[i][0]) * rates[1] +
+                (brackets[i][2] - brackets[i][1]) * rates[2] +
+                (income - brackets[i][2]) * rates[3];
+        }
+        else if (income <= brackets[i][4]) {
+            tax = brackets[i][0] * rates[0] +
+                (brackets[i][1] - brackets[i][0]) * rates[1] +
+                (brackets[i][2] - brackets[i][1]) * rates[2] +
+                (brackets[i][3] - brackets[i][2]) * rates[3] +
+                (income - brackets[i][3]) * rates[4];
+        }
+        else {
+            tax = brackets[i][0] * rates[0] +
+                (brackets[i][1] - brackets[i][0]) * rates[1] +
+                (brackets[i][2] - brackets[i][1]) * rates[2] +
+                (brackets[i][3] - brackets[i][2]) * rates[3] +
+                (brackets[i][4] - brackets[i][3]) * rates[4] +
+                (income - brackets[i][4]) * rates[5];
+        }
         return tax;
     }
 }
