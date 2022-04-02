aaa16fae2e0b0f59bf1d2707f6734246529fb644
Teerawat Chanvipardilok <82800595+Teerawat36167@users.noreply.github.com>
2022-04-02 23:36:46 +0700

    Week 10 Program 4

--- W10P4/W10P4.java
+++ W10P4/W10P4.java
@@ -1,13 +1,12 @@
 import java.io.File;
 import java.util.*;
-import java.io.*;
 
 public class W10P4 {
      public static void main(String[] args) {
 
-          ArrayList<Double> assistant = new ArrayList<>();
-		ArrayList<Double> associate = new ArrayList<>();
-		ArrayList<Double> full = new ArrayList<>();
+          ArrayList<Double> assistant = new ArrayList<Double>();
+		ArrayList<Double> associate = new ArrayList<Double>();
+		ArrayList<Double> full = new ArrayList<Double>();
 
           try {
                File text = new File("Salary.txt");
@@ -23,6 +22,26 @@ public class W10P4 {
                          full.add(salary);
                     }
                }
+               input.close();
+               Double totalAssistant = 0.0,totalAssociate = 0.0,totalFull = 0.0,totalAll = 0.0;
+               for (int i = 0; i < assistant.size(); i++) {
+                    totalAssistant += assistant.get(i);
+               }
+               for (int i = 0; i < associate.size(); i++) {
+                    totalAssociate += associate.get(i);
+               }
+               for (int i = 0; i < full.size(); i++) {
+                    totalFull += full.get(i);
+               }
+               totalAll = totalAssistant + totalAssociate + totalFull;
+               System.out.printf("Total salary for assistant professor is %.2f \n",totalAssistant);
+               System.out.printf("Total salary for associate professor is %.2f \n",totalAssociate);
+               System.out.printf("Total salary for full professor is %.2f \n",totalFull);
+               System.out.printf("Total salary for all professor is %.2f \n",totalAll);
+               System.out.printf("Average salary for assistant professor is %.2f \n",totalAssistant/assistant.size());
+               System.out.printf("Average salary for associate professor is %.2f \n",totalAssociate/associate.size());
+               System.out.printf("Average salary for full professor is %.2f \n",totalFull/full.size());
+               System.out.printf("Average salary for all professors is %.2f \n",totalAll/(assistant.size() + associate.size() + full.size()));
           } catch (Exception e) {
                e.printStackTrace();
           }
