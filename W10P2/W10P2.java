f4e351d4a37e1a82bce559a94735632e96e91a52
Teerawat Chanvipardilok <82800595+Teerawat36167@users.noreply.github.com>
2022-04-17 21:25:29 +0700

    Lab 11

--- W10P2/W10P2.java
+++ W10P2/W10P2.java
@@ -1,7 +1,7 @@
 public class W10P2 {
 	public static void main(String[] args) {
 		try {
-			Triangle triangle = new Triangle(1.0, 0.2, 0.5);
+			Triangle triangle = new Triangle(1.0, 1.0, 0.5);
 			System.out.println("The Triangle's area is " + triangle.getArea());
 		} catch (IllegalTriangleException e) {
 			e.printStackTrace();
